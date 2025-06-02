b6639aea9614f4dda302939e5c3a6e8fbf1752dc
nyanja1 <ogollanyanja@gmail.com>
2025-06-02 07:56:08 +0300

    added create tables

--- health_tracker/database.py
+++ health_tracker/database.py
@@ -1,7 +1,12 @@
+```python
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+from .models import Base
 
 # Create SQLite database
 engine = create_engine("sqlite:///health.db")
-
 Session = sessionmaker(bind=engine)
+
+# Create tables
+Base.metadata.create_all(engine)
+```
